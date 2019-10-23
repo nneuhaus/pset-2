239c9e2abb2334e4f2c3a3884e6f38503523cd9a
Nathalie Neuhaus <nneuhaus@ucvts.org>
2019-10-23 12:27:00 -0400

    fixed 5

--- src/perimeter.js
+++ src/perimeter.js
@@ -4,4 +4,5 @@ const Length_cm= Length_inches * 2.54
 let Width_inches= readlineSync.question("\nWhat is the width in inches?\n")
 const Width_cm=Width_inches * 2.54
 let Perimeter= Length_cm+Length_cm+Width_cm+Width_cm
+Perimeter = Perimeter.toLocaleString("en", { minimmumFractionDigits: 2, maximumFractionDigits: 2 });
 console.log("\nA(n) 8.5-by-11-inch sheet of paper has a perimeter of " + Perimeter + " centimeter(s).\n" );
