412c49b123184c793c1fbfe673511bf188df2a24
Nathalie Neuhaus <nathdancer@gmail.com>
2019-10-27 17:53:29 -0400

    Update trip.js

--- src/trip.js
+++ src/trip.js
@@ -1,12 +1,11 @@
 const readlineSync = require ("readline-sync")
-const Students= readlineSync.question("\nStudents:\n")
-const Teachers= readlineSync.question("\nTeachers: \n")
-const Capacity= readlineSync.question("\nBus Capacity: \n")
 
-const People=Students+Teachers
-const Number_of_full= People % Capacity
-const People_on_full= Number_of_full*Capacity
-const Remaining_people=People-People_on_full
-const Last_bus= Remaining_people
+let Students= readlineSync.question("\nStudents:\n")
+let Teachers= readlineSync.question("\nTeachers: \n")
+let Capacity= readlineSync.question("\nBus Capacity: \n")
 
-console.log(Number_of_full+ "\nbus(es) is (are) needed, with " + Last_bus + " passenger(s) on the last bus. \n")
+const People= parseInt(Students) + parseInt(Teachers)
+const Number_of_buses=Math.ceil(People/Capacity)
+const Remaining_people= People % Capacity
+
+console.log((Number_of_buses) + " bus(es) is (are) needed, with " + Remaining_people + " passenger(s) on the last bus. \n")
