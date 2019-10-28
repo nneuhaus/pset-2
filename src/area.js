7fd3716967f70a11a45fe6bd89ecbe6d37ea936c
Nathalie Neuhaus <nathdancer@gmail.com>
2019-10-27 22:36:22 -0400

    Update area.js

--- src/area.js
+++ src/area.js
@@ -1,7 +1,7 @@
 const readlineSync = require ("readline-sync")
-let Width_inches= readlineSync.question("\nWidth:\n")
+let Width_inches= readlineSync.question("\nWidth: ")
 const Width_mm= Width_inches * 25.4
-let Length_inches= readlineSync.question("\Length:\n")
+let Length_inches= readlineSync.question("\Length: ")
 const Length_mm= Length_inches * 25.4
 let Area= Length_mm*Width_mm
 Area = Area.toLocaleString("en", { minimmumFractionDigits: 2, maximumFractionDigits: 2 });
