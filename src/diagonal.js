370cb2b793f3af858838e77468b83b8bb5dbe322
Nathalie Neuhaus <nathdancer@gmail.com>
2019-10-27 22:33:35 -0400

    Update diagonal.js

--- src/diagonal.js
+++ src/diagonal.js
@@ -5,4 +5,4 @@ let Length_inches= readlineSync.question("\nLength:\n")
 const D= Length_inches + Width_inches
 let Diagonal= Math.hypot(Length_inches,Width_inches)
 Diagonal = Diagonal.toLocaleString("en", { minimmumFractionDigits: 2, maximumFractionDigits: 2 })
-console.log("\n"\nA(n)"+ Width_inches + "-by-" + Length_inches + "-inch sheet of paper has a diagonal of " + Diagonal + " inch(es).\n" );
+console.log("\nA(n)"+ Width_inches + "-by-" + Length_inches + "-inch sheet of paper has a diagonal of " + Diagonal + " inch(es).\n" );
